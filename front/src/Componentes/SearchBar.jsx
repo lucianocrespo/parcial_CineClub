a99fc0fcc4331e323838fc7cb00c13f733b32f32
Luciano <lucianocrespo94@gmail.com>
2026-08-19 20:29:23 -0300

    Agrego input de texto y botón buscar (SearchBar)

--- front/src/Componentes/SearchBar.jsx
+++ front/src/Componentes/SearchBar.jsx
@@ -1,8 +1,28 @@
-const SearchBar = () => {
+import { useState } from 'react'
+
+const SearchBar = ({ onSearch }) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault() // Evita que se recargue la página web al enviar el formulario
+    
+    // Busca si el input no está vacío
+    if (inputValue.trim() !== '') {
+      onSearch(inputValue)
+    }
+  }
+
   return (
-    <div>
-      <h2>Componente SearchBar</h2>
-    </div>
+    <form onSubmit={handleSubmit} style={{ marginBottom: '20px' }}>
+      <input
+        type="text"
+        placeholder="Buscar película..."
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        style={{ padding: '8px', width: '250px', marginRight: '10px' }}
+      />
+      <button type="submit" style={{ padding: '8px 15px' }}>Buscar</button>
+    </form>
   )
 }
 
